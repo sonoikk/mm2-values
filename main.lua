3a6f9faa3958f2eaa5b889f3855266ac1759c38e
sonoikk <mat382723@gmail.com>
2026-08-06 01:15:59 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -4,10 +4,14 @@ local Values = loadstring(game:HttpGet(
     "https://raw.githubusercontent.com/sonoikk/mm2-values/refs/heads/main/values.lua"
 ))()
 
-local CreateGUI = loadstring(game:HttpGet(
+print("Values loaded")
+
+local GUI = loadstring(game:HttpGet(
     "https://raw.githubusercontent.com/sonoikk/mm2-values/refs/heads/main/gui.lua"
 ))()
 
-CreateGUI(Values)
+print("GUI loaded")
+
+GUI(Values)
 
 print("MM2 Helper Loaded!")
