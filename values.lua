9371c487e5b24877d99780d2e8bbc901d922d86d
sonoikk <mat382723@gmail.com>
2026-08-05 23:11:55 +0300

    Update values.lua

--- values.lua
+++ values.lua
@@ -2,319 +2,319 @@ local Values = {
 
     Godlies = {
 
-        ["Traveler's Gun"] = {
-            Value = 5600,
-            Range = "N/A",
-            Stability = "Stable",
-            Demand = 5,
-            Rarity = 5,
-            Change = "+100"
-        },
+   ["Traveler's Gun"] = {
+        Value = 5600,
+        Range = "N/A",
+        Stability = "Stable",
+        Demand = 5,
+        Rarity = 5,
+        Change = "+100"
+   },
 
-        ["Evergun"] = {
-            Value = 3450,
-            Range = "N/A",
-            Stability = "Stable",
-            Demand = 5,
-            Rarity = 4,
-            Change = "-25"
-        },
+   ["Evergun"] = {
+        Value = 3450,
+        Range = "N/A",
+        Stability = "Stable",
+        Demand = 5,
+        Rarity = 4,
+        Change = "-25"
+   },
 
-        ["Constellation"] = {
-            Value = 2700,
-            Range = "N/A",
-            Stability = "Stable",
-            Demand = 5,
-            Rarity = 4,
-            Change = "-25"
-        },
+   ["Constellation"] = {
+        Value = 2700,
+        Range = "N/A",
+        Stability = "Stable",
+        Demand = 5,
+        Rarity = 4,
+        Change = "-25"
+   },
 
-        ["Evergreen"] = {
-            Value = 2500,
-            Range = "N/A",
-            Stability = "Stable",
-            Demand = 5,
-            Rarity = 4,
-            Change = "-25"
-        },
+   ["Evergreen"] = {
+        Value = 2500,
+        Range = "N/A",
+        Stability = "Stable",
+        Demand = 5,
+        Rarity = 4,
+        Change = "-25"
+   },
 
-        ["Turkey"] = {
-            Value = 2450,
-            Range = "N/A",
-            Stability = "Stable",
-            Demand = 5,
-            Rarity = 4,
-            Change = "-25"
-        },
+   ["Turkey"] = {
+        Value = 2450,
+        Range = "N/A",
+        Stability = "Stable",
+        Demand = 5,
+        Rarity = 4,
+        Change = "-25"
+   },
 
-        ["Vampire's Gun"] = {
-            Value = 1950,
-            Range = "N/A",
-            Stability = "Stable",
-            Demand = 5,
-            Rarity = 4,
-            Change = "+25"
-        },
+   ["Vampire's Gun"] = {
+        Value = 1950,
+        Range = "N/A",
+        Stability = "Stable",
+        Demand = 5,
+        Rarity = 4,
+        Change = "+25"
+   },
 
-        ["Alienbeam"] = {
-            Value = 1850,
-            Range = "N/A",
-            Stability = "Stable",
-            Demand = 5,
-            Rarity = 4,
-            Change = "-25"
-        },
+   ["Alienbeam"] = {
+        Value = 1850,
+        Range = "N/A",
+        Stability = "Stable",
+        Demand = 5,
+        Rarity = 4,
+        Change = "-25"
+   },
 
-        ["Darkshot"] = {
-            Value = 1700,
-            Range = "1700 - 1725",
-            Stability = "Doing Well",
-            Demand = 6,
-            Rarity = 4,
-            Change = "+25"
-        },
+   ["Darkshot"] = {
+        Value = 1700,
+        Range = "1700 - 1725",
+        Stability = "Doing Well",
+        Demand = 6,
+        Rarity = 4,
+        Change = "+25"
+   },
 
-        ["Darksword"] = {
-            Value = 1675,
-            Range = "1675 - 1700",
-            Stability = "Doing Well",
-            Demand = 6,
-            Rarity = 4,
-            Change = "+25"
-        },
+   ["Darksword"] = {
+        Value = 1675,
+        Range = "1675 - 1700",
+        Stability = "Doing Well",
+        Demand = 6,
+        Rarity = 4,
+        Change = "+25"
+   },
 
-        ["Raygun"] = {
-            Value = 1550,
-            Range = "1550 - 1600",
-            Stability = "Overpaid For",
-            Demand = 6,
-            Rarity = 3,
-            Change = "+50"
-        },
+   ["Raygun"] = {
+        Value = 1550,
+        Range = "1550 - 1600",
+        Stability = "Overpaid For",
+        Demand = 6,
+        Rarity = 3,
+        Change = "+50"
+   },
 
-        ["Blossom"] = {
-            Value = 1330,
-            Range = "1330 - 1350",
-            Stability = "Doing Well",
-            Demand = 6,
-            Rarity = 4,
-            Change = "+10"
-        },
+   ["Blossom"] = {
+        Value = 1330,
+        Range = "1330 - 1350",
+        Stability = "Doing Well",
+        Demand = 6,
+        Rarity = 4,
+        Change = "+10"
+   },
 
-        ["Sakura"] = {
-            Value = 1320,
-            Range = "1320 - 1340",
-            Stability = "Doing Well",
-            Demand = 6,
-            Rarity = 4,
-            Change = "+10"
-        },
+   ["Sakura"] = {
+        Value = 1320,
+        Range = "1320 - 1340",
+        Stability = "Doing Well",
+        Demand = 6,
+        Rarity = 4,
+        Change = "+10"
+   },
 
-        ["Sunrise"] = {
-            Value = 1125,
-            Range = "N/A",
-            Stability = "Stable",
-            Demand = 5,
-            Rarity = 4,
-            Change = "+25"
-        },
+   ["Sunrise"] = {
+        Value = 1125,
+        Range = "N/A",
+        Stability = "Stable",
+        Demand = 5,
+        Rarity = 4,
+        Change = "+25"
+   },
 
-        ["Snowcannon"] = {
-            Value = 850,
-            Range = "N/A",
-            Stability = "Stable",
-            Demand = 5,
-            Rarity = 3,
-            Change = "+25"
-        },
+   ["Snowcannon"] = {
+        Value = 850,
+        Range = "N/A",
+        Stability = "Stable",
+        Demand = 5,
+        Rarity = 3,
+        Change = "+25"
+   },
 
-        ["Bauble"] = {
-            Value = 825,
-            Range = "N/A",
-            Stability = "Stable",
-            Demand = 5,
-            Rarity = 4,
-            Change = "-25"
-        },
+   ["Bauble"] = {
+        Value = 825,
+        Range = "N/A",
+        Stability = "Stable",
+        Demand = 5,
+        Rarity = 4,
+        Change = "-25"
+   },
 
-        ["Sunset"] = {
-            Value = 625,
-            Range = "N/A",
-            Stability = "Stable",
-            Demand = 5,
-            Rarity = 3,
-            Change = "+25"
-        },
+   ["Sunset"] = {
+        Value = 625,
+        Range = "N/A",
+        Stability = "Stable",
+        Demand = 5,
+        Rarity = 3,
+        Change = "+25"
+   },
 
-        ["Soul"] = {
-            Value = 615,
-            Range = "N/A",
-            Stability = "Stable",
-            Demand = 5,
-            Rarity = 3,
-            Change = "+5"
-        },
+   ["Soul"] = {
+        Value = 615,
+        Range = "N/A",
+        Stability = "Stable",
+        Demand = 5,
+        Rarity = 3,
+        Change = "+5"
+   },
 
-        ["Spirit"] = {
-            Value = 605,
-            Range = "N/A",
-            Stability = "Stable",
-            Demand = 5,
-            Rarity = 3,
-            Change = "+5"
-        },
+   ["Spirit"] = {
+        Value = 605,
+        Range = "N/A",
+        Stability = "Stable",
+        Demand = 5,
+        Rarity = 3,
+        Change = "+5"
+   },
 
-        ["Rainbow Gun"] = {
-            Value = 420,
-            Range = "N/A",
-            Stability = "Stable",
-            Demand = 5,
-            Rarity = 3,
-            Change = "+5"
-        },
+   ["Rainbow Gun"] = {
+        Value = 420,
+        Range = "N/A",
+        Stability = "Stable",
+        Demand = 5,
+        Rarity = 3,
+        Change = "+5"
+   },
 
-        ["Flora"] = {
-            Value = 410,
-            Range = "N/A",
-            Stability = "Stable",
-            Demand = 5,
-            Rarity = 3,
-            Change = "+10"
-        },
+   ["Flora"] = {
+        Value = 410,
+        Range = "N/A",
+        Stability = "Stable",
+        Demand = 5,
+        Rarity = 3,
+        Change = "+10"
+   },
 
         ["Rainbow"] = {
-            Value = 410,
-            Range = "N/A",
-            Stability = "Stable",
-            Demand = 5,
-            Rarity = 3,
-            Change = "+5"
-        },
+        Value = 410,
+        Range = "N/A",
+        Stability = "Stable",
+        Demand = 5,
+        Rarity = 3,
+        Change = "+5"
+   },
 
-        ["Bloom"] = {
-            Value = 400,
-            Range = "N/A",
-            Stability = "Stable",
-            Demand = 5,
-            Rarity = 3,
-            Change = "+10"
-        },
-        ["Watergun"] = {
-    Value = 250,
-    Range = "N/A",
-    Stability = "Stable",
-    Demand = 3,
-    Rarity = 3,
-    Change = "-10"
-},
+   ["Bloom"] = {
+        Value = 400,
+        Range = "N/A",
+        Stability = "Stable",
+        Demand = 5,
+        Rarity = 3,
+        Change = "+10"
+   },
+   ["Watergun"] = {
+        Value = 250,
+        Range = "N/A",
+        Stability = "Stable",
+        Demand = 3,
+        Rarity = 3,
+        Change = "-10"
+   },
 
-["Icecream"] = {
-    Value = 160,
-    Range = "150 - 165",
-    Stability = "Fluctuating",
-    Demand = 3,
-    Rarity = 3,
-    Change = "-40"
-},
+   ["Icecream"] = {
+        Value = 160,
+        Range = "150 - 165",
+        Stability = "Fluctuating",
+        Demand = 3,
+        Rarity = 3,
+        Change = "-40"
+   },
 
-["Treat"] = {
-    Value = 155,
-    Range = "N/A",
-    Stability = "Stable",
-    Demand = 3,
-    Rarity = 3,
-    Change = "-5"
-},
+   ["Treat"] = {
+        Value = 155,
+        Range = "N/A",
+        Stability = "Stable",
+        Demand = 3,
+        Rarity = 3,
+        Change = "-5"
+   },
 
-["Beachy"] = {
-    Value = 150,
-    Range = "140 - 155",
-    Stability = "Fluctuating",
-    Demand = 3,
-    Rarity = 3,
-    Change = "-10"
-},
+   ["Beachy"] = {
+        Value = 150,
+        Range = "140 - 155",
+        Stability = "Fluctuating",
+        Demand = 3,
+        Rarity = 3,
+        Change = "-10"
+   },
 
-["Sands"] = {
-    Value = 150,
-    Range = "140 - 155",
-    Stability = "Fluctuating",
-    Demand = 3,
-    Rarity = 3,
-    Change = "-10"
-},
+   ["Sands"] = {
+        Value = 150,
+        Range = "140 - 155",
+        Stability = "Fluctuating",
+        Demand = 3,
+        Rarity = 3,
+        Change = "-10"
+   },
 
-["Sweet"] = {
-    Value = 150,
-    Range = "N/A",
-    Stability = "Stable",
-    Demand = 3,
-    Rarity = 3,
-    Change = "-5"
-},
+   ["Sweet"] = {
+        Value = 150,
+        Range = "N/A",
+        Stability = "Stable",
+        Demand = 3,
+        Rarity = 3,
+        Change = "-5"
+   },
 
-["Borealis"] = {
-    Value = 145,
-    Range = "N/A",
-    Stability = "Stable",
-    Demand = 3,
-    Rarity = 3,
-    Change = "-5"
-},
+   ["Borealis"] = {
+        Value = 145,
+        Range = "N/A",
+        Stability = "Stable",
+        Demand = 3,
+        Rarity = 3,
+        Change = "-5"
+   },
 
-["Australis"] = {
-    Value = 140,
-    Range = "N/A",
-    Stability = "Stable",
-    Demand = 3,
-    Rarity = 3,
-    Change = "-5"
-},
+   ["Australis"] = {
+        Value = 140,
+        Range = "N/A",
+        Stability = "Stable",
+        Demand = 3,
+        Rarity = 3,
+        Change = "-5"
+   },
 
-["Bat"] = {
-    Value = 120,
-    Range = "115 - 125",
-    Stability = "Fluctuating",
-    Demand = 2,
-    Rarity = 2,
-    Change = "-5"
-},
+   ["Bat"] = {
+        Value = 120,
+        Range = "115 - 125",
+        Stability = "Fluctuating",
+        Demand = 2,
+        Rarity = 2,
+        Change = "-5"
+   },
 
-["Pearlshine"] = {
-    Value = 85,
-    Range = "N/A",
-    Stability = "Stable",
-    Demand = 2,
-    Rarity = 3,
-    Change = "-5"
-},
+   ["Pearlshine"] = {
+        Value = 85,
+        Range = "N/A",
+        Stability = "Stable",
+        Demand = 2,
+        Rarity = 3,
+        Change = "-5"
+   },
 
-["Pearl"] = {
-    Value = 80,
-    Range = "N/A",
-    Stability = "Stable",
-    Demand = 2,
-    Rarity = 3,
-    Change = "-5"
-},
+   ["Pearl"] = {
+        Value = 80,
+        Range = "N/A",
+        Stability = "Stable",
+        Demand = 2,
+        Rarity = 3,
+        Change = "-5"
+   },
 
-["Candy"] = {
-    Value = 80,
-    Range = "N/A",
-    Stability = "Stable",
-    Demand = 1,
-    Rarity = 2,
-    Change = "-5"
-},
+   ["Candy"] = {
+        Value = 80,
+        Range = "N/A",
+        Stability = "Stable",
+        Demand = 1,
+        Rarity = 2,
+        Change = "-5"
+   },
 
-["Heartblade"] = {
-    Value = 65,
-    Range = "N/A",
-    Stability = "Stable",
-    Demand = 1,
-    Rarity = 2,
-    Change = "-2"
-},
+   ["Heartblade"] = {
+        Value = 65,
+        Range = "N/A",
+        Stability = "Stable",
+        Demand = 1,
+        Rarity = 2,
+        Change = "-2"
+    },
 
     ["Luger"] = {
         Value = 40,
@@ -685,8 +685,69 @@ local Values = {
         Change = "0"
     },
 
-    
-},
+    ["Cookieblade"] = {
+         Value = 3,
+         Range = "N/A",
+         Stability = "Stable",
+         Demand = 1,
+         Rarity = 2,
+         Change = "0"
+    },
+
+    ["Blue Seer"] = {
+         Value = 3,
+         Range = "N/A",
+         Stability = "Stable",
+         Demand = 1,
+         Rarity = 1,
+         Change = "0"
+    },
+
+    ["Purple Seer"] = {
+         Value = 3,
+         Range = "N/A",
+         Stability = "Stable",
+         Demand = 1,
+         Rarity = 1,
+         Change = "0"
+    },
+
+    ["Red Seer"] = {
+         Value = 3,
+         Range = "N/A",
+         Stability = "Stable",
+         Demand = 1,
+         Rarity = 1,
+         Change = "0"
+    },
+
+    ["Seer"] = {
+         Value = 3,
+         Range = "N/A",
+         Stability = "Stable",
+         Demand = 1,
+         Rarity = 1,
+         Change = "0"
+    },
+
+    ["Orange Seer"] = {
+         Value = 2,
+         Range = "N/A",
+         Stability = "Stable",
+         Demand = 1,
+         Rarity = 1,
+         Change = "0"
+    },
+
+    ["Yellow Seer"] = {
+         Value = 2,
+         Range = "N/A",
+         Stability = "Stable",
+         Demand = 1,
+         Rarity = 1,
+         Change = "0"
+    },
+        
     Ancients = {
 
     },
