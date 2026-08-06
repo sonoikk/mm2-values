cc3b9ad44dc86b0b4b0a2cfc67b762a36573a46e
sonoikk <mat382723@gmail.com>
2026-08-06 23:56:50 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 print("Loading MM2 Helper...")
 
 local Values = loadstring(game:HttpGet(
-    "https://raw.githubusercontent.com/sonoikk/mm2-values/main/values.lua"
+    "https://raw.githubusercontent.com/sonoikk/mm2-values/refs/heads/main/values.lua"
 ))()
 
 local Offers = loadstring(game:HttpGet(
